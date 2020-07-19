8519390dda7d19d174b7eba3f45ec0c83e390395
xiaozhaolnsy <xiaozhaolnsy@126.com>
2020-07-19 21:04:12 +0800

    Day2 express 路由优化

--- exercise-code/app.js
+++ exercise-code/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const r = require('./routes/post');
+// const r = require('./routes/post');
+const routes = require('./routes/post');
+
 
 app.use(express.json());
-app.use('/r',r);
+// app.use('/r',r);
 
+routes(app);
 
 app.listen(port,()=>{
     console.log(`Express server listening at http://localhost:${port}`);
