26ac3d958d23dfe9d4ed22a44ee3fa79c9576c8e
xiaozhaolnsy <xiaozhaolnsy@126.com>
2020-07-20 14:41:42 +0800

    node mysql

--- Project_AttendanceDay/attendance-day-node/util/db.js
+++ Project_AttendanceDay/attendance-day-node/util/db.js
@@ -3,20 +3,17 @@ const mysql = require('mysql');
 module.exports = {
     //配置
     config:{
-        host:'10.10.100.20',
-        port:'3306',
-        user:'root',
-        password:'123456',
-        database:'attendance_day'
+        host: '10.10.100.20',
+        port: '3306',
+        user: 'node',
+        password: '123456',
+        database: 'attendance_day'
     },
     //连接
     sqlConnect:function(sql,sqlArr,callBack){
         var pool = mysql.createPool(this.config)
         pool.getConnection((err,conn)=>{
-            console.log('12345');
-            if(err){
-                console.log('conn err');return;
-            }
+            if(err){console.log(err);return;}
             //驱动回调
             conn.query(sql,sqlArr,callBack);
             //释放连接
