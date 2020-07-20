9c91f91cb5a507b4f1687613c3c835c3540762f8
xiaozhaolnsy <xiaozhaolnsy@126.com>
2020-07-20 14:24:20 +0800

    Day3 node环境

--- Project_AttendanceDay/attendance-day-node/routes/users.js
+++ Project_AttendanceDay/attendance-day-node/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../util/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  var sql = 'select * from usr_master';
+  var arr = [];
+  var callBack = (err,data)=>{
+    if(err){console.log('db error')}
+    else{res.send({'list': data})}
+  }
+  db.sqlConnect(sql,arr,callBack);
+
 });
 
 module.exports = router;
